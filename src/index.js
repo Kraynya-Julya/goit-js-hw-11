7a602b38fecad144830b41e9b95eca6e62033f88
Крайняя Юлия <kraynya1991@gmail.com>
2022-03-23 12:11:41 +0200

    add

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import './sass/main.scss';
+import NewApi from './js/fatchImages';
+import card from './hbs/card.hbs';
+import Notiflix from 'notiflix';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    articlesContainer: document.querySelector('.gallery'),
+    loadMore: document.querySelector('.load-more'),
+}
+const newApi = new NewApi();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+  newApi.newWord = e.target.elements.searchQuery.value.trim();
+  clearImageCards();
+  newApi.getImages().then(({ hits, totalHits }) => {
+
+    if (newApi.newWord === '') {
+     Notiflix.Notify.info(`Enter any word`);
+    } else if (hits.length === 0) {
+        onFetchError();
+       } else {
+          renderImageCards(hits);
+          Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+       }
+      });
+    }
+
+    function clearImageCards() {
+           refs.articlesContainer.innerHTML = '';
+      
+    }
+// newsApiService.getImages().then(({ hits, totalHits }) => {
+//   if (newsApiService.searchWord === '') {
+//     Notiflix.Notify.info(`Enter any word`)
+//   } else if (hits.length === 0) {
+//     onFetchError();
+//   } else {
+//     loadMoreBtn.show();
+//     renderImageCards(hits);
+//     Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+//     lightbox.refresh();
+//   }
+// });
+function renderImageCards (word) {
+    refs.articlesContainer.insertAdjacentHTML('beforeend', card(word));
+
+}
+
+function onFetchError(error) {
+    // console.log(error);
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');
+}
